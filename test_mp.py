2e0c084cdf5ddd2b646c00781973e35a99eb282c
MasterKenobi7 <marco.henriquespereira@uzh.ch>
2022-02-18 16:29:54 +0100

    Create test_mp.py

---
+++ test_mp.py
@@ -0,0 +1,24 @@
+# conda activate test
+# cd Desktop/Hackathon
+# python3 test.py
+
+# packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import nltk
+# import sklearn as sk
+
+# from collections import Counter
+# from sklearn.datasets import load_iris
+# from sklearn.model_selection import train_test_split
+
+# print("Hello World")
+
+# test
+df = pd.read_csv("Task/production_line_optimization_first_log.csv")
+print(df)
+# df['SQRT Balance'] = np.sqrt((df['Balance']))
+# df.plot(x="Balance", y=["Deposits", "Withdrawls"])
+# plt.show()
+# plt.savefig("figs/test.png")
