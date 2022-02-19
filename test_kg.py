2971453c660e21fc3440c18ad279cf2be4623c89
MasterKenobi7 <marco.henriquespereira@uzh.ch>
2022-02-19 10:02:22 +0100

    edf

--- test_kg.py
+++ test_kg.py
@@ -16,29 +16,27 @@ Created on Fri Feb 18 16:58:53 2022
 # print(df)
 
 
-
 #############################
 
 import pandas as pd
 import numpy as np
 
-np.random.seed(7)
+np.random.seed(8)
 
-one = np.arange(1,22)
+one = np.arange(1, 22)
 two = np.arange(22, 40)
-three = np.arange(40,66)
-four = np.arange(66,74)
+three = np.arange(40, 66)
+four = np.arange(66, 74)
 
-one_rand = np.random.choice(one, size = 21, replace = False)
-two_rand = np.random.choice(two, size = len(two), replace = False)
-three_rand = np.random.choice(three, size = len(three), replace = False)
-four_rand = np.random.choice(four, size = len(four), replace = False)
+one_rand = np.random.choice(one, size=21, replace=False)
+two_rand = np.random.choice(two, size=len(two), replace=False)
+three_rand = np.random.choice(three, size=len(three), replace=False)
+four_rand = np.random.choice(four, size=len(four), replace=False)
 
 print(one_rand)
 print(two_rand)
 print(three_rand)
 print(four_rand)
-
 
 
 job_order = {"A": one_rand,
