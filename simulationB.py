179e851eeaf1d443e0d3c5fc78c664da9718d818
flavio eisenring <flavio.eisenring@outlook.com>
2022-02-20 10:50:59 +0100

    Update simulationB.py

--- simulationB.py
+++ simulationB.py
@@ -1,6 +1,8 @@
 from modulefinder import Module
 import random
+from operator import itemgetter 
 emptyIndex = 0
+offsetLine = 21
 pT1 = [emptyIndex,1674,1897,1974,1623,1937,2417,1777,2255,1841 ,796,2090,2020,1558,2321,2202,2329,3533,3434]
 pT2 = [emptyIndex,3716,2584,2619,3016,3520,3136,3202,3108,2966,2172,4014,2439, 796,2917,3224,2847,4815, 796]
 pT3 = [emptyIndex,2818 ,796,2540, 796,2698, 796,2064,2068,2362,1920,2051,1992,2575,2389,2533, 796,3690,4243]
@@ -8,7 +10,7 @@ pT4 = [emptyIndex,2562,2665,3494,5391,2420,2790,3090,3246,3226,2588,2351,2957,23
 pT5 = [emptyIndex,2862,3174,3472,2632,2415,3068,2880, 796,2391,3145, 796,2867,2790,2579,2593,3448,4417,4098]
 pT6 = [emptyIndex,2195,2035,1561,1927,1522,1704,2133,2030,2146,1493, 398,1475,1960,1857,1453, 398,2762,2879] #1613
 
-jobId = list(range(22-21,39+1-21))
+jobId = list(range(22-offsetLine,39+1-offsetLine))
 jobWaitingLounge = [],
 triedCombinations =[]
 
@@ -157,7 +159,8 @@ for iteration in range(1,100000):
             if time < quickestTime:
                 quickestTime = time
                 quickestId = jobId.copy()
-                print(quickestId); print(quickestTime)
+                quickestId = [x+offsetLine for x in quickestId]
+                print(quickestId); print(itemgetter(*jobId.copy())(pT1)); print(quickestTime); print("Iteration:"); print(iteration)
             break          
 
 print("final result:")        
